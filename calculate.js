32688a00d20192f98d00d87d7e7e79b2f404af39
Muntasir Ahmed <muntasirahmed@Muntasirs-MacBook-Air.local>
2022-02-17 14:01:25 +0600

    css done

---
+++ calculate.js
@@ -0,0 +1,10 @@
+function Input(input) {
+    const Input = document.getElementById(input + '-input')
+    let InputValue=parseFloat(foodInput.value)
+    
+}
+document
+  .getElementById("calculate-button")
+  .addEventListener("click", function () {
+    console.log("i am clicked");
+  });
